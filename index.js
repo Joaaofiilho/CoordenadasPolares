b22406fc88b1a7deedd3cc18f10d621e301ae935
Caio Pinho <akiracaio@edu.unifor.br>
2019-03-31 04:39:43 -0300

    Criado a classe Topico
    A classe Topico vai servir agora para informar qual id a mensagem e se o ponto será ou não colocado no circulo mais interno ou externo.

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 
 
-fazerCirculoBolinhas = (raio, numBolinhas, desenharCirculo) => {
-    var bolinhas = []
-    var bolinhasCirculo = document.createElement('div')
+class Topico {
+
+    constructor(id, isCategoria, informacao){
+        this.id = id;
+        this.isCategoria = isCategoria;
+        this.informacao = informacao;
+        this.bolinhasRelacionadas = [];
+    }
+
+    adicionarTopico(topico){
+        this.bolinhasRelacionadas.push(topico);
+    }
+
+    mostrarTopico() {
+        this.bolinhasRelacionadas.forEach(topico => {
+            console.log('====================================');
+            console.log(topico.informacao);
+            console.log('====================================');
+        });
+    }
+
+}
+
+
+
+popularVetorTopicos = () => {
+    const topicos = [];
+
+    topicos.push(new Topico(1, true, "bolinha 1"));
+    topicos.push(new Topico(2, true, "bolinha 1"));
+    topicos.push(new Topico(3, true, "bolinha 1"));
+    topicos.push(new Topico(4, true, "bolinha 1"));
+    topicos.push(new Topico(5, false, "bolinha 1"));
+    topicos.push(new Topico(6, false, "bolinha 1"));
+    topicos.push(new Topico(7, false, "bolinha 1"));
+    topicos.push(new Topico(8, false, "bolinha 1"));
+    topicos.push(new Topico(9, false, "bolinha 1"));
+    topicos.push(new Topico(10, false, "bolinha 1"));
+    topicos.push(new Topico(11, false, "bolinha 1"));
+    topicos.push(new Topico(12, false, "bolinha 1"));
+    topicos.push(new Topico(13, false, "bolinha 1"));
+    topicos.push(new Topico(14, false, "bolinha 1"));
+    topicos.push(new Topico(15, false, "bolinha 1"));
+
+    return topicos;
+}
+
+//TODO: Organizar o metodo fazerCirculo para agora ele ler o ID dos vetor de topicos e modifica-lo para que ele adicione todos os potos na tela de uma unica vez.
+fazerCirculoBolinhas = (raio, numBolinhas, desenharCirculo, id) => {
+
+    const bolinhas = []
+    const bolinhasCirculo = document.createElement('div')
     bolinhasCirculo.classList.add('bolinhasContainer')
+
     if(desenharCirculo)
         bolinhasCirculo.classList.add('circulo')
     
@@ -11,33 +61,61 @@ fazerCirculoBolinhas = (raio, numBolinhas, desenharCirculo) => {
     bolinhasCirculo.style.height = raio*2 + "px"
 
     document.body.appendChild(bolinhasCirculo)
+      
 
-    var bolinha = document.createElement('div')
-    bolinha.classList.add('bolinha')
 
-    for(var angulo = 0; angulo <= 360; angulo += 360/numBolinhas) {
-        var newB = bolinha.cloneNode(false)
-    
-        var x = raio * Math.cos((Math.PI*angulo)/180)
-        var y = raio * Math.sin((Math.PI*angulo)/180)
-    
-        var transform = `translate(${x}px, ${y}px)`
-        newB.style.transform = transform
+
+    let iterratorID = id;
+    for(var angulo = 0; angulo < 360; angulo += 360/numBolinhas) {
         
-        bolinhas.push(newB)
+        const bolinhaBase = null;
+
+        //simulando receber um topico
+        if(angulo == 72){
+            bolinhaBase = new Topico(iterratorID++,raio,"bolinhas");
+            bolinhaBase.adicionarTopico(new Topico(0,raio,"bolinhas"));
+            bolinhaBase.adicionarTopico(new Topico(1,raio,"bolinhas"));
+        }else{
+            bolinhaBase = new Topico(iterratorID++,raio,"bolinhas");
+        }    
+        
+        
+        const bolinha = document.createElement('div' + bolinhaBase.id);
+        bolinha.classList.add('bolinha');
+
+        const x = raio * Math.cos((Math.PI*angulo)/180);
+        const y = raio * Math.sin((Math.PI*angulo)/180);
+    
+        const transform = `translate(${x}px, ${y}px)`;
+        bolinha.style.transform = transform;
+        
+        bolinhas.push(bolinha);
     }
 
     bolinhas.forEach(bolinha => {
         bolinha.addEventListener('click', (event) => {
+            console.log('====================================');
+            console.log("CLICK NA BOLINHA");
+            console.log('====================================');
+            
             if (bolinha.classList.contains('selecionada'))
                 bolinha.classList.remove('selecionada')
             else
-                bolinha.classList.add('selecionada')    
+            bolinha.classList.add('selecionada')    
         })
 
         bolinhasCirculo.appendChild(bolinha)
     })
+
+    return iterratorID;
+
 }
 
-fazerCirculoBolinhas(150, 5, true)
-fazerCirculoBolinhas(300, 20, false)
+printarTudo = () => {
+    let iterradorID = 0;
+    
+    iterradorID = fazerCirculoBolinhas(400, 10, false, iterradorID);
+    iterradorID = fazerCirculoBolinhas(150, 5, true, iterradorID);
+}
+
+printarTudo();
